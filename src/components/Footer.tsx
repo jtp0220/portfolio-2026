9632972f33dd77b717b47803d3b9ac1184341fcf
Jeremy Tran Perez <jeremytranperez@gmail.com>
2026-01-23 18:36:34 -0500

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import "../styles/index.css";
 
 export default function Footer() {
     return (
-        <footer className="bg-bg-primary border-primary flex items-center justify-center border-t py-10">
+        <footer className="bg-bg-primary border-text-primary/20 flex items-center justify-center border-t py-10">
             <p className="text-text-primary">© 2026 Jeremy Tran Perez. All rights reserved.</p>
         </footer>
     );
