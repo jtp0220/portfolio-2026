8f1a228d2cd3125bb98b3e858fdd92c8df7ccb60
Jeremy Tran Perez <jeremytranperez@gmail.com>
2026-03-25 14:48:11 -0400

    Added photos to Photos section

--- src/components/Photography.tsx
+++ src/components/Photography.tsx
@@ -1,25 +1,25 @@
 import "../styles/index.css";
-const images = Object.values(import.meta.glob("../assets/gallery/*.jpg", { eager: true })).map((mod: any) => mod.default);
+const images = Object.values(import.meta.glob("../assets/gallery/*.{jpg,JPG}", { eager: true })).map((mod: any) => mod.default);
 
 function PhotoFrame({ src }: { src: string }) {
-    return (
-        <div className="bg-text-primary h-auto w-70 p-2">
-            <img src={src} alt="" onContextMenu={(e) => e.preventDefault()} />
-        </div>
-    );
+  return (
+    <div className="bg-text-primary h-auto w-70 p-2">
+      <img src={src} alt="" onContextMenu={(e) => e.preventDefault()} />
+    </div>
+  );
 }
 
 export default function Photography() {
-    return (
-        <section id="photography" className="bg-bg-secondary text-text-primary scroll-mt-15">
-            <div className="m-auto flex max-w-250 flex-col items-center justify-center px-7 py-10">
-                <h1 className="text-text-primary mb-10 text-3xl font-bold">Photography</h1>
-                <div className="flex w-full flex-wrap items-center justify-center gap-10">
-                    {images.map((src, index) => (
-                        <PhotoFrame key={index} src={src} />
-                    ))}
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section id="photography" className="bg-bg-secondary text-text-primary scroll-mt-15">
+      <div className="m-auto flex max-w-250 flex-col items-center justify-center px-7 py-10">
+        <h1 className="text-text-primary mb-10 text-3xl font-bold">Photography</h1>
+        <div className="flex w-full flex-wrap items-center justify-center gap-10">
+          {images.map((src, index) => (
+            <PhotoFrame key={index} src={src} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
