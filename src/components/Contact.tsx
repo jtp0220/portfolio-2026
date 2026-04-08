0928f4eefb77ade80fc75485313a22d510f174e2
Jeremy Tran Perez <jeremytranperez@gmail.com>
2026-04-08 18:46:47 -0400

    added message to Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,7 +8,8 @@ export default function Contact() {
       <div className="m-auto flex max-w-250 flex-col items-center justify-center px-7 py-10">
         <h1 className="text-text-primary mb-10 text-3xl font-bold">Contact</h1>
         <div className="">
-          <div className="text-text-primary bg-bg-secondary hover:bg-text-secondary flex items-center justify-center gap-5 rounded-2xl p-5">
+          <p className="pb-10 text-center text-lg">If you'd like to get in touch, feel free to email me at: </p>
+          <div className="text-text-primary bg-bg-secondary hover:bg-bg-tertiary border-text-secondary flex items-center justify-center gap-5 rounded-2xl border p-5">
             <EmailIcon className="size-9" />
             <a href="mailto:jeremytranperez@gmail.com">
               <span className="text-lg">jeremytranperez@gmail.com</span>
